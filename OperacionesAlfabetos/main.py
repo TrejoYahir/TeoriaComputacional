61bc8d18bd4adb7c0b4aae23f3a8f16ae275341b
Elioth Monroy Martos <eliothmonroy@yahoo.com.mx>
2016-08-20 00:54:36 -0500

    Actualizado a python 3

--- OperacionesAlfabetos/main.py
+++ OperacionesAlfabetos/main.py
@@ -1,35 +1,27 @@
 #Creamos los menus
-# -*- coding: utf-8 -*-
 mensaje_entrada = "Hola, por favor ingresa el tipo de Alfabeto con el que trabajare:\n"
 menu = ('1.- Ingresar alfabeto manualmente\n'
 	'2- Alfabeto Espaniol\n'
 	'3.- Numeros (0...9)\n')
-print mensaje_entrada + menu
+print (mensaje_entrada + menu)
 opcion = input()
-print opcion
-alfabeto=[]
-if opcion==1:
-	seguir=1;
-	while seguir:
-		simbolo=raw_input("Ingrese una letra\n")
+print (opcion)
+alfabeto = []
+if opcion == '1':
+	print ("Entre al if")
+	seguir = 1
+	while seguir==1:
+		simbolo=input("Ingrese un simbolo\n")
 		alfabeto.append(simbolo);
-		if len(alfabeto)>3:
-			while opcion11:	
-				print 
-				opcion2 = raw_input("Desea ingresar más elementos s/n \n")
-				if opcion2==s:
-					pass
-					opcion11=0
-				elif opcion2==n:
+		if len(alfabeto)>2:
+			seguir_agregando=1
+			while seguir_agregando==1:	
+				opcion2 = input("Desea ingresar más elementos s/n \n")
+				if opcion2=='s':
+					seguir_agregando=0
+				elif opcion2=='n':
 					seguir=0
-					opcion11=0
+					seguir_agregando=0
 				else:
 					pass
-	print alfabeto			
-
-
-
-
-
-		
-	
+	print (alfabeto)
