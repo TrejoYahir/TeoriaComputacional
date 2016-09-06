eb8f52278ff7ab1dae401c1fab3a685446563dc3
Elioth Monroy Martos <eliothmonroy@yahoo.com.mx>
2016-09-05 20:10:21 -0500

    Iniciando Práctica 2

---
+++ OperacionesLenguages/main.py
@@ -0,0 +1,64 @@
+#Función principal
+def main():
+	mensaje_entrada = "Inciso a) de la práctica\nHola, por favor ingresa el tipo de Alfabeto con el que trabajare:\n"
+	menu = ('1.- Ingresar alfabeto manualmente\n'
+	'2- Alfabeto Espaniol\n'
+	'3.- Numeros (0...9)\n')
+	#Imprimimos el menu
+	print (mensaje_entrada)
+	#Creamos el alfabeto
+	alfabeto=crear_alfabeto(menu)
+	#Al final imprimimos el alfabeto
+	print ("El alfabeto con el que trabajare es:\n")
+	print (alfabeto)
+	print("\n")
+
+#Función que crea el alfabeto dependiendo de la opción del usuario
+def crear_alfabeto(menu):
+	opcion_valida=True
+	while opcion_valida:
+		#Imprimimos el menu y obtenemos la opción
+		opcion = input(menu)
+		#Esto es un Set en python
+		alfabeto = set()
+		#Dependiendo la opción es el menu a usar
+		# 1- Alfabeto dinamico ingresado por el usuario
+		if opcion == '1':
+			opcion_valida=False
+			seguir = True
+			while seguir:
+				simbolo=input("Ingrese un simbolo\n")
+				if simbolo != "" and (" " in simbolo)==False:
+					alfabeto.add(simbolo)
+				else:
+					print("Caracter no valido\n")
+				if len(alfabeto)>2:
+					seguir_agregando=True
+					while seguir_agregando:	
+						opcion2 = input("Desea ingresar más elementos? s/n \n")
+						if opcion2=='s':
+							seguir_agregando=False
+						elif opcion2=='n':
+							seguir=False
+							seguir_agregando=False
+							return alfabeto
+						else:
+							pass
+		#Alfabeto español
+		elif opcion == '2':
+			opcion_valida=False
+			print ("El alfabeto es espaniol")
+			alfabeto={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'}
+			return alfabeto
+		#Alfabeto númerico
+		elif opcion == '3':
+			opcion_valida=False
+			print ("El alfabeto es numerico")
+			alfabeto={'0','1','2','3','4','5','6','7','8','9'}
+			return alfabeto
+		#Opción invalida se repite el ciclo
+		else:
+			print('Opcion no valida, debe ser alguna de las siguientes\n')
+
+#Ejecutamos el programa
+main()
