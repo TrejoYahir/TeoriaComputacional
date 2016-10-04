5356e07fdb237235d02ed5a8c2717ff0de1a57da
Elioth Monroy Martos <eliothmonroy@yahoo.com.mx>
2016-10-03 22:31:01 -0500

    Clase automata

---
+++ AutomataLexicoJava/automata.py
@@ -0,0 +1,8 @@
+class automata():
+	#Definimos el estado del automata como el inicial q0
+	estado=0
+	def calcular_estado(self, q):
+		if estado==0:
+			estado=estado_cero(q)
+		elif 
+	def estado_cero(q):
