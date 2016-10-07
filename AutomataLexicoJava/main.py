f763a7ef296fd07499af722bb55ff92292c0703a
Elioth Monroy Martos <eliothmonroy@yahoo.com.mx>
2016-10-06 22:47:01 -0500

    Primera version del automata

--- AutomataLexicoJava/main.py
+++ AutomataLexicoJava/main.py
@@ -1,6 +1,10 @@
+from automata import Automata
 def main():
+	auto=Automata()
 	archivo=open("prueba.java","r")
 	for line in archivo.readlines():
-		if "=" in line:
-			print (line)
+		if ("0" in line or "1" in line or "2" in line or "3" in line or "4" in line or "5" in line or "6" in line or "7" in line or "8" in line or "9" in line):
+			for c in line:
+				auto.calcular_estado(c)
+			print(auto.estado)
 main()
--- AutomataLexicoJava/prueba.java
+++ AutomataLexicoJava/prueba.java
@@ -1,10 +1,10 @@
 public class prueba{
 	public static void main(String[] args) {
 		int octall=-0123;
-		short dato=12A00;
+		short dato=0xabe;
 		float pi=3.1416, cteGrav=-6.674E-11;
 		int hexal=0xx12;
-                double e = 1.23E+3;
+		double e = 1.23E+03;
 		for (int i= 0129;i<45;i++){
 			dato*=200;
 			System.out.print("dato= "+(dato+0xAA));
