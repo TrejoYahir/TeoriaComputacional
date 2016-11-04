03bfbf77966c620928e446a1810ecb7c93f952ec
Elioth Monroy Martos <eliothmonroy@yahoo.com.mx>
2016-11-04 00:11:52 -0600

    Minusculas con acento y lo de Yahir :heart:

--- AFND-Aplicado/automata.py
+++ AFND-Aplicado/automata.py
@@ -1,7 +1,7 @@
 class Automata():
 	#Definimos el automata
 	mayusculas=("A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z")
-	minusculas=("a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z")
+	minusculas=("a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","á","é","í","ó","ú")
 	estado=["q0"]
 	estado_aux=[]
 	def calcular_estado(self,q):
